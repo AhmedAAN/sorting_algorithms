31c123296a101a214bbf8370d795c709e3437b4c
AhmedAAN <ahmed.a.nagy@outlook.com>
2023-09-23 11:46:11 -0700

    0-bubble_sort.c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int i, j, temp;
+	bool modified;
+
+	for (i = 0; i < (int) size - 1; i++){
+		modified = false;
+		for (j = 0; j < (int) size - i - 1; j++){
+			if (array[j] > array[j + 1]){
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+				modified = true;
+			}
+		}
+		if (modified == false)
+			break;
+	}
+}
