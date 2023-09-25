db07fd5a90b885bf4362718a47af40c1c50ba7c4
AhmedAAN <ahmed.a.nagy@outlook.com>
2023-09-25 15:19:18 -0700

    3-quick_sort.c modified

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -34,18 +34,18 @@ int lomuto_partition(int *array, int low, int high, size_t size)
 		{
 			i++;
 			if (i != j)
-            {
-                swap(&array[i], &array[j]);
-                print_array(array, size);
-            }
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
 		}
 	}
 
 	if (i + 1 != high)
-    {
-        swap(&array[i + 1], &array[high]);
-        print_array(array, size);
-    }
+	{
+		swap(&array[i + 1], &array[high]);
+		print_array(array, size);
+	}
 
 	return (i + 1);
 }
